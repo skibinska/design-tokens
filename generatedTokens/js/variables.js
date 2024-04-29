9b1f85948847a54f031afddae97398232af3eed2
Ewelina Skibinska <e.skibinska@wellcome.org>
2024-04-29 10:57:52 +0100

    Add varaibles.js

---
+++ generatedTokens/js/variables.js
@@ -0,0 +1,18 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 29 Apr 2024 09:56:29 GMT
+ */
+
+export const BreakpointXs = "512px";
+export const BreakpointSm = "768px";
+export const BreakpointMd = "1024px";
+export const BreakpointLg = "1200px";
+export const ColorRed10 = "#f9cdca";
+export const ColorRed20 = "#ed858e";
+export const ColorRed05 = "#fff0f2";
+export const ColorBlack = "#000000";
+export const ColorWhite = "#ffffff";
+export const SpaceStaticXxs = 4;
+export const SpaceStaticXs = 8;
+export const SpaceStaticSm = 16;
+export const SpaceStaticMd = 32;
