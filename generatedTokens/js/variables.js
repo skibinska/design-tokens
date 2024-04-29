eb6e702b1d2635e039189c0b7fd288381726c3ad
Ewelina Skibinska <e.skibinska@wellcome.org>
2024-04-29 11:13:24 +0100

    Split input JSON into files

--- generatedTokens/js/variables.js
+++ generatedTokens/js/variables.js
@@ -1,12 +1,13 @@
 /**
  * Do not edit directly
- * Generated on Mon, 29 Apr 2024 09:58:28 GMT
+ * Generated on Mon, 29 Apr 2024 10:16:04 GMT
  */
 
 export const BreakpointXs = "512px";
 export const BreakpointSm = "768px";
 export const BreakpointMd = "1024px";
 export const BreakpointLg = "1200px";
+export const BreakpointXl = "1344px";
 export const ColorRed10 = "#f9cdca";
 export const ColorRed20 = "#ed858e";
 export const ColorRed05 = "#fff0f2";
